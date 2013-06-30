cca2d88b14c72cfbf9c9d21bc3467c95fcd65618
HopeAsd <qfeizaijun@qq.com>
2013-06-30 16:19:35 +0800

    修改一下Food的父类

--- src/sinocraft/core/items/SCFood.java
+++ src/sinocraft/core/items/SCFood.java
@@ -1,15 +1,11 @@
 package sinocraft.core.items;
 
+import cbproject.core.item.CBCGenericItem;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import sinocraft.SinoCraft;
 import net.minecraft.client.renderer.texture.IconRegister;
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemStack;
-import net.minecraft.potion.PotionEffect;
-import net.minecraft.world.World;
 
 /**
  * @author HopeAsd
@@ -17,40 +13,50 @@ import net.minecraft.world.World;
  */
 
 public class SCFood extends ItemFood {
-	public SCFood(int par1, int par2, float par3, boolean par4) {
-		super(par1, par2, par3, par4);
+	
+	private String iconName = "";
+
+	public SCFood(int par1, int par2, boolean par3) {
+		super(par1, par2, par3);
 		this.setCreativeTab(SinoCraft.sct);
 	}
-
-	public void onFoodEaten(ItemStack par1ItemStack, World par2World,
-			EntityPlayer par3EntityPlayer) {
-		super.onFoodEaten(par1ItemStack, par2World, par3EntityPlayer);
-		// 在这里添加addPotionEffect方法
+	public SCFood(int par1,int par2,float par3,boolean par4){
+		super(par1,par2,par3,par4);
+		this.setCreativeTab(SinoCraft.sct);
 	}
-
-	public void addPotionEffect(int par1PotionID, int time, int xiaoguo,
-			World par4World, EntityPlayer par5EntityPlayer) {
-		if (xiaoguo >= 2) {
-			xiaoguo = 0;
-			if (!par4World.isRemote && par4World.rand.nextFloat() < 0.1F) {
-				par5EntityPlayer.addPotionEffect(new PotionEffect(par1PotionID,
-						time * 20, xiaoguo));
-			}
-		}
-
+	public SCFood(int par1, int par2,float par3, boolean par4,String name) {
+		super(par1, par2, par3,par4);
+		this.setIAndU(name);
+		this.setCreativeTab(SinoCraft.sct);
 	}
-
-	public ItemStack onEaten(ItemStack par1ItemStack, World par2World,
-			EntityPlayer par3EntityPlayer) {
-		super.onEaten(par1ItemStack, par2World, par3EntityPlayer);
-		// 修改吃好后的掉落物
-		return new ItemStack(Item.bowlEmpty);
+	public SCFood(int par1, int par2, boolean par3,String name) {
+		super(par1, par2, par3);
+		this.setIAndU(name);
+		this.setCreativeTab(SinoCraft.sct);
 	}
-
-	@SideOnly(Side.CLIENT)
+	
+	public SCFood setIconName(String name) {
+		this.iconName = name;
+		return this;
+	}
+	
+	/**
+	 * 推荐，有助于名字统一 同时设置UnlocalizedName和IconName
+	 * 
+	 * @param names
+	 *            名字
+	 */
+	public SCFood setIAndU(String names) {
+		this.setUnlocalizedName(names);
+		this.setIconName(names);
+		return this;
+	}
+	
 	@Override
-	public void registerIcons(IconRegister par1IconRegister) {
-		this.itemIcon = par1IconRegister.registerIcon("sinocraft:"
-				+ this.getUnlocalizedName());
+	@SideOnly(Side.CLIENT)
+	public void registerIcons(IconRegister i)
+	{
+		itemIcon = i.registerIcon("SinoCraft:"+iconName);
 	}
+
 }
